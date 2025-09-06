e2ab85c0348fd1f4e9a7c1ae18bc487ee00f86d8
svasquezg7 <svasquezg7@miumg.edu.gt>
2025-09-06 17:27:27 -0600

    Agregar Comentario

--- src/app/components/agregar-editar-comentario/agregar-editar-comentario.ts
+++ src/app/components/agregar-editar-comentario/agregar-editar-comentario.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Comentario } from '../../services/comentario';
 
 @Component({
   selector: 'app-agregar-editar-comentario',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './agregar-editar-comentario.html',
   styleUrl: './agregar-editar-comentario.scss'
 })
 export class AgregarEditarComentario {
+  agregarComentario: FormGroup;
+
+constructor(private readonly fb: FormBuilder) {
+  this.agregarComentario = this.fb.group({
+    titulo: ['', Validators.required],
+    creador: ['',Validators.required],
+    texto: ['',Validators.required]
+  });
+}
+
+agregar(){
+    console.log(this.agregarComentario.value)  
+  const nuevoComentario: Comentario ={
+    titulo: this.agregarComentario.value?.titulo,
+    creador: this.agregarComentario.value?.creador,
+    texto: this.agregarComentario.value?.texto,
+    fechaCreacion: new Date()
+  };
+  console.log(nuevoComentario);
+}
 
 }
+
